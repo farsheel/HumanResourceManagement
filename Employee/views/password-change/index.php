0205f23331df691bc982b3d02bea1291301adea6
anusreemm <mmanusree95@gmail.com>
2017-06-20 19:56:03 +0530

    employee password

--- Employee/views/password-change/index.php
+++ Employee/views/password-change/index.php
@@ -1,9 +1,4 @@
 <?php
 /* @var $this yii\web\View */
 ?>
-<h1>password-change/index</h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
