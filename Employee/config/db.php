d5bab94c989e1422cd93e92b5d0b39fb4bc3d476
Vishnu Prasad vs <vishnuprasad.vs58@gmail.com>
2017-06-20 13:25:01 +0530

    db employee set password

--- Employee/config/db.php
+++ Employee/config/db.php
@@ -1,9 +1,11 @@
 <?php
 
+$config = parse_ini_file(__DIR__ . '/../../../config.ini', true);
+
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=hrm',
-    'username' => 'root',
-    'password' => '123',
+    'username' => $config['mysql_usr'],
+    'password' => $config['mysql_pwd'],
     'charset' => 'utf8',
 ];
--- config/db.php
+++ config/db.php
@@ -5,12 +5,7 @@ $config = parse_ini_file(__DIR__ . '/../../config.ini', true);
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=hrm',
-<<<<<<< HEAD
     'username' => $config['mysql_usr'],
     'password' => $config['mysql_pwd'],
-=======
-    'username' => 'root',
-    'password' => '123',
->>>>>>> c665aaabbd4672b2a8e94fb1fa3b171cd7b90c30
     'charset' => 'utf8',
 ];
